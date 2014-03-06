2cfa0471049f01ac70bb41e560ac3febcca82c17
Laurent VB <lvanbass@gmail.com>
2014-03-06 10:26:23 +0100

    Properly find a field among the paths *and* virtuals

--- views/helpers.js
+++ views/helpers.js
@@ -1,7 +1,8 @@
 var moment = require('moment'),
     _ = require('underscore'),
     ejs = require('ejs'),
-    fs = require('fs');
+    fs = require('fs'),
+    utils = require('../lib/utils');
 
 var helpers = {
     _ : _,
@@ -19,12 +20,13 @@ module.exports.init = function(admin){
          * @returns {string}
          */
         res.locals.url = require('../lib/utils').url.bind(res.locals, req);
+        res.locals.getField = require('../lib/utils').getField;
 
         res.locals.req = req;
 
         res.locals.tableFormat = function(fieldName, document){
             var value = document[fieldName],
-                field = document.schema.paths[fieldName] || document.schema.virtuals[fieldName];
+                field = utils.getField(document, fieldName);
 
             if (!field) {
                 throw new Error('Unknown field %s in model %s', fieldName, document.schema.modelName);
@@ -38,7 +40,7 @@ module.exports.init = function(admin){
                 return moment(value).format('YYYY/MM/DD hh:mm:ss');
             }
 
-            if (field.options && field.options.type.name == 'ObjectId'){
+            if (field.options && field.options.ref){
                 return res.locals.label(value, field.options.ref);
             }
 
@@ -50,10 +52,10 @@ module.exports.init = function(admin){
             if (labelField = admin.models[modelName].label){
                 return _.isFunction(labelField) ? labelField(document) : document[labelField];
             }
-            if ('label' in document.schema.paths && document.schema.paths.label.options.type == String){
+            if ((labelField = utils.getField(document, 'label')) && labelField.options.type == String){
                 return document.label;
             }
-            if ('name' in document.schema.paths && document.schema.paths.name.options.type == String){
+            if ((labelField = utils.getField(document, 'name')) && labelField.options.type == String){
                 return document.name;
             }
             return document;
