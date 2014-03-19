af68668b5588c8e30a98060fdf356a3dd36307d4
Laurent VB <lvanbass@gmail.com>
2014-03-19 18:22:16 +0100

    Enum fields are select boxes

--- example/models/Record.js
+++ example/models/Record.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var RecordSchema = new mongoose.Schema({
     name: {type: String, required: true},
     published: {type: Date},
+    type: {type: String, enum: ['LP', 'EP', 'Single']},
     artist : { type: mongoose.Schema.Types.ObjectId, ref: 'Artist', required: true },
     songs: [String]
 });
